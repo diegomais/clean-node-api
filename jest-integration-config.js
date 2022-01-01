5b2ed6ff2a7f944f09e4caefb28195d847afade1
Diego Mais <diegomais.dev@gmail.com>
2022-01-01 20:09:12 -0300

    🔧 create custom configurations for unit and integration tests

---
+++ jest-integration-config.js
@@ -0,0 +1,5 @@
+const config = require('./jest.config')
+
+config.testMatch = ['**/*.test.js']
+
+module.exports = config
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: '@shelf/jest-mongodb'
+}
